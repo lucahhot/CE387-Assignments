128350a959d674eba1d0c96cbd33aeb7ed6c0547
mitch weng <tosaponweng@gmail.com>
2024-02-29 16:19:22 -0600

    multiply module

---
+++ final_project/sv/multiply.sv
@@ -0,0 +1,66 @@
+module multiply #(
+    parameter DATA_SIZE,
+    parameter DATA_SIZE_2
+)(
+    input logic clock,
+    input logic reset,
+    output logic in_rd_en,
+    input logic in_empty,
+    output logic out_wr_en,
+    input logic out_full,
+    input logic [DATA_SIZE - 1:0] x,
+    input logic [DATA_SIZE - 1:0] y,
+    output logic [DATA_SIZE - 1:0] dout
+);
+
+typedef enum logic [0:0] {S0, S1} state_types;
+state_types state, next_state;
+
+logic [DATA_SIZE - 1:0] mult_out, mult_out_c;
+logic [DATA_SIZE - 1:0] q_x, q_y;
+logic [DATA_SIZE_2 - 1:0] temp;
+
+function int QUANTIZE(int i);
+    QUANTIZE = i <<< BITS;
+endfunction
+
+function int DEQUANTIZE(int i);
+    DEQUANTIZE = i >>> BITS;
+endfunction
+
+// ASSUME QUANTIZED VALUES, IF NOT QUANTIZED THEN ADD QUANTIZE FUNCTION INTO IT
+function int MULTIPLY_FIXED(int x, int y);
+    logic signed [DATA_SIZE_2 - 1:0] intermediate;
+
+    intermediate = x * y;
+
+    MULTIPLY_FIXED = DATA_SIZE'(DEQUANTIZE(intermediate));   
+endfunction
+
+always_ff @(posedge clock or posedge reset) begin
+    if (reset == 1'b1) begin
+        mult_out <= '0;
+    end else begin
+        mult_out <= mult_out_c;
+    end
+end
+
+always_comb begin
+
+    if (in_empty == 1'b0) begin
+        q_x = QUANTIZE(x);
+        q_y = QUANTIZE(y);
+        mult_out_c = MULTIPLY_FIXED(q_x, q_y);
+        out_wr_en = 1'b1;
+        in_rd_en = 1'b1;
+        dout = mult_out;
+    end else begin
+        out_wr_en = 1'b0;
+        in_rd_en = 1'b0;
+        mult_out_c = '0;
+        dout = '0;
+    end
+
+end
+
+endmodule
--- final_project/uvm/fm_radio_globals.sv
+++ final_project/uvm/fm_radio_globals.sv
@@ -3,23 +3,39 @@
 
 // UVM Globals
 localparam CLOCK_PERIOD = 10;
-localparam CORDIC_DATA_WIDTH = 16;
 localparam BITS = 10;
 localparam QUANT_VAL = (1 << BITS);
 localparam M_PI = 3.14159265358979323846;
+localparam DATA_SIZE = 32;
+localparam DATA_SIZE_2 = 64;
 localparam string FILE_OUT_NAME = "../source/uvm_test_output.txt";
 
-// QUANTIZE_F function
-function int QUANTIZE_F(shortreal i);
-    QUANTIZE_F = int'(shortreal'(i) * shortreal'(QUANT_VAL));
+// NOT SYNTHESIZABLE! FIXED POINTS ARE JUST REPRESENTED WITH INTS
+// // QUANTIZE_F function
+// function int QUANTIZE_F(shortreal i);
+//     QUANTIZE_F = int'(shortreal'(i) * shortreal'(QUANT_VAL));
+// endfunction
+
+// // DEQUANTIZE_F function
+// function shortreal DEQUANTIZE_F(int i);
+//     DEQUANTIZE_F = shortreal'(shortreal'(i) / shortreal'(QUANT_VAL));
+// endfunction
+
+function int QUANTIZE(int i);
+    QUANTIZE = i <<< BITS;
 endfunction
 
-// DEQUANTIZE_F function
-function shortreal DEQUANTIZE_F(int i);
-    DEQUANTIZE_F = shortreal'(shortreal'(i) / shortreal'(QUANT_VAL));
+function int DEQUANTIZE(int i);
+    DEQUANTIZE = i >>> BITS;
 endfunction
 
-function int multiply_int(int x, int y);
+// ASSUME QUANTIZED VALUES, IF NOT QUANTIZED THEN ADD QUANTIZE FUNCTION INTO IT
+function int MULTIPLY_FIXED(int x, int y);
+    logic signed [DATA_SIZE_2 - 1:0] intermediate;
+
+    intermediate = x * y;
+
+    MULTIPLY_FIXED = DATA_SIZE'(DEQUANTIZE(intermediate));
     
 endfunction
 
