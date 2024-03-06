c5a5cbb3c6731320778e646152ef1dcd4d0ef971
Luc Ah-Hot <luc.ahhot@gmail.com>
2024-03-05 20:55:23 -0600

    Fixed add and sub

--- Final_Project/sv/sub.sv
+++ Final_Project/sv/sub.sv
@@ -14,17 +14,54 @@ module sub (
     output  logic [DATA_SIZE-1:0] out_din  
 );
 
+typedef enum logic {S0, S1} state_types;
+state_types state, next_state;
+
+logic [DATA_SIZE-1:0] diff, diff_c; 
+
+always_ff @(posedge clock or posedge reset) begin
+    if (reset == 1'b1) begin
+        state <= S0;
+        diff <= '0;
+    end else begin
+        state <= next_state;
+        diff <= diff_c;
+    end
+end
+
 always_comb begin
     x_in_rd_en = 1'b0;
     y_in_rd_en = 1'b0;
     out_wr_en = 1'b0;
-    // Reading values and writing them directly
-    if (x_in_empty == 1'b0 && y_in_empty == 1'b0 && out_full == 1'b0) begin
-        x_in_rd_en = 1'b1;
-        y_in_rd_en = 1'b1;
-        out_wr_en = 1'b1;
-        out_din = $signed(x_in_dout) - $signed(y_in_dout);
+    next_state <= state;
+    diff_c <= diff;
+
+    case(state)
+
+    S0: begin
+        if (x_in_empty == 1'b0 && y_in_empty == 1'b0) begin
+            x_in_rd_en = 1'b1;
+            y_in_rd_en = 1'b1;
+            diff_c = $signed(x_in_dout) - $signed(y_in_dout);
+        end
     end
+
+    S1: begin
+        if (out_full == 1'b0) begin
+            out_wr_en = 1'b1;
+            out_din = diff;
+        end
+    end
+
+    default: begin
+        x_in_rd_en = 1'b0;
+        y_in_rd_en = 1'b0;
+        out_wr_en = 1'b0;
+        diff_c = 'X;
+        out_din = 'X;
+    end
+    
+    endcase
 end
 
 endmodule
